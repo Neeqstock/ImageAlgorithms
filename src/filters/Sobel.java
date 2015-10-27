5d3ee6e552541877b56c2e59b183b889ebf3f4ef
Acer <Acer@10.87.123.210>
2015-10-27 17:31:28 +0100

    qsedfjasgh

--- src/filters/Sobel.java
+++ src/filters/Sobel.java
@@ -2,7 +2,20 @@ package filters;
 
 public class Sobel implements Ifilters{
 
-	/** Sobel custom Kernel */
+	int[] verticalValues = {1,0,-1,2,0,-2,1,0,-1};
+	int[] horizontalValues = {1,2,1,0,0,0,-1,-2,-1};
+	
+	/** Vertical Sobel */ 
+	public int[][] generateVerticalSobel(){
+		return generateKernel(3, 3, verticalValues);
+	}
+	
+	/** Horizontal Sobel */
+	public int[][] generateHorizontalSobel(){
+		return generateKernel(3, 3, horizontalValues);
+	}
+	
+	/** Sobel custom Kernel implementation*/
 	@Override
 	public int[][] generateKernel(int rows, int columns, int[] values) {
 		int[][] kernel = new int[rows][columns];
