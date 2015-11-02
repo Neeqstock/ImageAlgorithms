5c9708704eb2bbaa3a8b72aed35485ccb90d4e2e
Nicola <Nicola@NeequeHP>
2015-11-02 15:33:16 +0100

    Gesu.replaceAll

--- src/user_interface/Engine.java
+++ src/user_interface/Engine.java
@@ -1,6 +1,8 @@
 package user_interface;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import pgm_utilities.PGMImage;
 import pgm_utilities.PGMUtilities;
@@ -10,53 +12,57 @@ import filterAlgorithms.AlgSobel;
 import filterAlgorithms.IAlgorithm;
 
 public class Engine {
-	
+
 	private IAlgorithm selectedAlgorithm = new AlgSobel(); // XXX DEFAULT
 	private ArrayList<String> selectedFiles = new ArrayList<String>();
-	
+
 	public Engine() {
-		
+
 	}
-	
+
 	public void setSelectedAlgorithm(IAlgorithm selectedAlgorithm) {
 		this.selectedAlgorithm = selectedAlgorithm;
 	}
-	
-	public void setAlgorithmByName(String name){
-		switch(name){
-			case "Sobel":
-				selectedAlgorithm = new AlgSobel();
-				break;
-			case "Box":
-				selectedAlgorithm = new AlgBox();
-				break;
-			case "Sharpening":
-				selectedAlgorithm = new AlgSharpening();
-			default:
-				break;
+
+	public void setAlgorithmByName(String name) {
+		switch (name) {
+		case "Sobel":
+			selectedAlgorithm = new AlgSobel();
+			break;
+		case "Box":
+			selectedAlgorithm = new AlgBox();
+			break;
+		case "Sharpening":
+			selectedAlgorithm = new AlgSharpening();
+		default:
+			break;
 		}
 	}
-	
+
 	public void setSelectedFiles(ArrayList<String> selectedFiles) {
 		this.selectedFiles = selectedFiles;
 	}
-	
-	public void compute(){
+
+	public void compute() {
 		ArrayList<PGMImage> images = new ArrayList<PGMImage>();
-		
+
 		for (int i = 0; i < selectedFiles.size(); i++) {
 			images.add(PGMUtilities.readPGM(selectedFiles.get(i)));
 		}
-		
+
 		for (int i = 0; i < selectedFiles.size(); i++) {
-			selectedAlgorithm.computeImage(images.get(i), getNameToken(selectedFiles.get(i)));
+			selectedAlgorithm.computeImage(images.get(i),
+					getNameToken(selectedFiles.get(i)));
 		}
-		
+
 	}
-	
-	private String getNameToken(String filepath){
-		return "cucu"; // TODO
+
+	private String getNameToken(String filepath) {
+		// StringTokenizer tokenizer = new StringTokenizer()
+		File file = new File(filepath);
+		String gesu = file.getName();
+		gesu.replaceAll(".pgm", "");
+		return gesu;
 	}
-	
-	
+
 }
