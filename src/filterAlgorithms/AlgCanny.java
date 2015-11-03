2e74ae10a0d09cc77d302993a2c0b1bf97acecf0
Acer <Acer@10.87.123.210>
2015-11-03 17:48:49 +0100

    lj

--- src/filterAlgorithms/AlgCanny.java
+++ src/filterAlgorithms/AlgCanny.java
@@ -1,12 +1,28 @@
 package filterAlgorithms;
 
+import java.awt.image.BufferedImage;
+
 import pgm_utilities.PGMImage;
+import canny.CannyEdgeDetector;
 
 public class AlgCanny implements IAlgorithm{
 
 	@Override
 	public void computeImage(PGMImage image, String name) {
-		// TODO Auto-generated method stub
+		//create the detector
+		CannyEdgeDetector detector = new CannyEdgeDetector();
+
+		//adjust its parameters as desired
+		detector.setLowThreshold(0.5f);
+		detector.setHighThreshold(1f);
+
+		//apply it to an image
+		
+		BufferedImage img = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+		
+		detector.setSourceImage(image);
+		detector.process();
+		BufferedImage edges = detector.getEdgesImage();
 		
 	}
 	
