a03f5e6d7589da09f9b1d734de194c4f861eb76f
Acer <Acer@10.87.123.210>
2015-11-09 14:16:37 +0100

    noise fatto, bisogna modificare l'interfaccia (passare un double a Salt&Pepper,Uniform e Impulsive, e passarne due al Gaussian) Negli algoritmi ci sono i get e set

--- src/filterAlgorithms/AlgImpulseNoise.java
+++ src/filterAlgorithms/AlgImpulseNoise.java
@@ -1,12 +1,46 @@
 package filterAlgorithms;
 
-import pgm_utilities.PGMImage;
+import java.util.Random;
 
-public class AlgImpulseNoise implements IAlgorithm{
+import pgm_utilities.PGMImage;
+import pgm_utilities.PGMUtilities;
+
+public class AlgImpulseNoise implements IAlgorithm {
+
+	private double density;
+
+	//STUB
+	public AlgImpulseNoise() {
+		density = 0.05;
+	}
+
+	public AlgImpulseNoise(double density) {
+		this.density = density;
+	}
+
+	public void setDensity(double density) {
+		this.density = density;
+	}
+
+	public double getDensity() {
+		return density;
+	}
 
 	@Override
 	public void computeImage(PGMImage image, String name) {
-		// TODO Auto-generated method stub
+		int[] newImg = image.getPixels();
+		int imgSize = newImg.length;
+		int affectedPixels = (int) (((double) imgSize) * density);
+		Random rnd = new Random();
+
+		for (int i = 0; i < affectedPixels; i++) {
+			int nextIndex = rnd.nextInt(imgSize);
+			newImg[nextIndex] = 255;
+		}
+		PGMImage filteredImage = new PGMImage(image.getWidth(), image.getHeight(), image.getMax_val());
+		filteredImage.setPixels(newImg);
+		
+		PGMUtilities.writePGM(filteredImage, PGMUtilities.standardOutputPath + name + "_impulsiveNoise.pgm");
 		
 	}
 }
--- src/filterAlgorithms/AlgUniformNoise.java
+++ src/filterAlgorithms/AlgUniformNoise.java
@@ -1,11 +1,49 @@
 package filterAlgorithms;
 
+import java.util.Random;
+
 import pgm_utilities.PGMImage;
+import pgm_utilities.PGMUtilities;
 
 public class AlgUniformNoise implements IAlgorithm{
+	
+	private double k;
+	
+	//STUB
+	public AlgUniformNoise(){
+		k = 10;
+	}
+	
+	public AlgUniformNoise(double k){
+		this.k = k;
+	}
+	
+	public void setK(double k) {
+		this.k = k;
+	}
+	public double getK() {
+		return k;
+	}
+	
 @Override
 public void computeImage(PGMImage image, String name) {
-	// TODO Auto-generated method stub
+	int[] out = image.getPixels();
+	Random rnd = new Random();
+	for (int i = 0; i < out.length; i++) {
+		double noise = 2*k*(rnd.nextDouble() - 0.5);
+		out[i] = out[i] + (int)noise;
+		
+		if (out[i] > 255) {
+			out[i] = 255;
+		}
+		
+		if (out[i] < 0) {
+			out[i] = 0;
+		}
+	}
+	PGMImage filteredImage = new PGMImage(image.getWidth(), image.getHeight(), image.getMax_val());
+	filteredImage.setPixels(out);
 	
+	PGMUtilities.writePGM(filteredImage, PGMUtilities.standardOutputPath + name + "_UniformNoise.pgm");
 }
 }
